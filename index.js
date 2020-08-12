3f90b88889da0e4726bc384e545373af750fd331
Harman Singh <h286sing@uwaterloo.ca>
2020-08-11 21:56:04 -0600

    updated index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const {World, Render, Runner, Bodies, Engine} = Matter;
+const {World, Render, Runner, Bodies, Engine, Body, Events} = Matter;
 
 const engine = Engine.create(); // gives a world object
+engine.world.gravity.y = 0;
 
 const {world} = engine
 
-const height = 600;
-const width = 600;
-const cells = 3;
+const height = window.innerHeight;
+const width = window.innerWidth;
+const cellHorizontal = 10;
+const cellVertical = 10;
 
 const render = Render.create({
     element: document.body,
@@ -23,28 +25,30 @@ Runner.run(Runner.create(), engine)
 
 
 // Borders
-World.add(world, Bodies.rectangle(width / 2, 0, width, 40, {isStatic: true}));
-World.add(world, Bodies.rectangle(width / 2, height, width, 40, {isStatic: true}));
-World.add(world, Bodies.rectangle(0, height / 2, 40, height, {isStatic: true}));
-World.add(world, Bodies.rectangle(width, height / 2, 40, height, {isStatic: true}));
+World.add(world, Bodies.rectangle(width / 2, 0, width, 2, {isStatic: true}));
+World.add(world, Bodies.rectangle(width / 2, height, width, 2, {isStatic: true}));
+World.add(world, Bodies.rectangle(0, height / 2, 2, height, {isStatic: true}));
+World.add(world, Bodies.rectangle(width, height / 2, 2, height, {isStatic: true}));
 
 // Making the maze
-const grid = Array(cells).fill(null).map(() => Array(cells).fill(false));
+const grid = Array(cellVertical).fill(null).map(() => Array(cellHorizontal).fill(false));
 
 // Vertical
-const vertical = Array(cells).fill(null).map(() => Array(cells - 1).fill(false));
+const vertical = Array(cellVertical).fill(null).map(() => Array(cellHorizontal - 1).fill(false));
 
 // Horizontal
-const horizontal = Array(cells - 1).fill(null).map(() => Array(cells).fill(false));
+const horizontal = Array(cellVertical - 1).fill(null).map(() => Array(cellHorizontal).fill(false));
 
-const startRow = Math.floor(Math.random() * cells);
-const startColumn = Math.floor(Math.random() * cells);
+const startRow = Math.floor(Math.random() * cellVertical);
+const startColumn = Math.floor(Math.random() * cellHorizontal);
+const unitLengthX = width / cellHorizontal;
+const unitLengthY = height / cellVertical;
 
 const arrayShuffle = (arr) => {
     counter = arr.length;
 
     while(counter > 0) {
-        index = Math.floor(Math.random() * counter);
+        const index = Math.floor(Math.random() * counter);
 
         counter --;
 
@@ -73,7 +77,7 @@ const recurse = (row, column) => {
     for (let neighbour of neighbours) {
         const [nextRow, nextColumn, direction] = neighbour;
 
-        if(nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+        if(nextRow < 0 || nextRow >= cellVertical || nextColumn < 0 || nextColumn >= cellHorizontal) {
             continue;
         }
         if(grid[nextRow][nextColumn]) {
@@ -89,7 +93,7 @@ const recurse = (row, column) => {
         else if(direction === "up") {
             horizontal[row - 1][column] = true;
         }
-        else if(direction === "left") {
+        else if(direction === "down") {
             horizontal[row][column] = true;
         }
 
@@ -99,5 +103,113 @@ const recurse = (row, column) => {
 
 recurse(startRow, startColumn);
 
-console.log(vertical);
-console.log(horizontal);
+horizontal.forEach((row, rowIndex) => {
+    row.forEach((open, colIndex) => {
+        if(open) {
+            return;
+        } else {
+            const wall = Bodies.rectangle(
+                colIndex * unitLengthX + unitLengthX / 2,
+                rowIndex * unitLengthY + unitLengthY,
+                unitLengthX,
+                5,
+                {
+                    label: "wall",
+                    render: {
+                        fillStyle: 'red'
+                    },
+                    isStatic: true
+                }
+            );
+            World.add(world, wall);
+        }
+    })
+})
+
+vertical.forEach((row, rowIndex) => {
+    row.forEach((open, colIndex) => {
+        if(open) {
+            return;
+        }
+        else {
+            const wall = Bodies.rectangle(
+                colIndex * unitLengthX + unitLengthX,
+                rowIndex * unitLengthY + unitLengthY / 2,
+                5,
+                unitLengthY,
+                {
+                    label: "wall",
+                    render: {
+                        fillStyle: 'red'
+                    },
+                    isStatic: true
+                }
+            )
+            World.add(world, wall);
+        }
+    })
+})
+
+// Goal
+const goal = Bodies.rectangle(
+    width - unitLengthX / 2,
+    height - unitLengthY / 2,
+    unitLengthX * .7, 
+    unitLengthY * .7,
+    {
+        isStatic: true,
+        render: {
+            fillStyle: 'green'
+        },
+        label: "goal"
+    }
+);
+World.add(world, goal)
+
+// Ball
+const ball = Bodies.circle(
+    unitLengthX / 2,
+    unitLengthY / 2,
+    Math.min(unitLengthX, unitLengthY) / 4, {
+        label: "ball",
+        render: {
+            fillStyle: 'blue'
+        }
+    }
+);
+
+World.add(world, ball);
+
+document.addEventListener("keydown", (event) => {
+    const {x, y} = ball.velocity;
+    
+    if(event.keyCode === 87) {
+        Body.setVelocity(ball, {x, y: y - 5});
+    }
+    else if (event.keyCode === 65) {
+        Body.setVelocity(ball, {x: x - 5, y});
+    }
+    else if (event.keyCode === 83) {
+        Body.setVelocity(ball, {x, y: y + 5});
+    }
+    else if (event.keyCode === 68) {
+        Body.setVelocity(ball, {x: x + 5, y});
+    }
+})
+
+// Ball collided with the Goal
+Events.on(engine, "collisionStart", event => {
+    event.pairs.forEach((collision) => {
+        const labels = ['ball', 'goal'];
+        if(labels.includes(collision.bodyA.label)
+         && 
+         labels.includes(collision.bodyB.label)) {
+            world.gravity.y = 1;
+            world.bodies.forEach((shape) => {
+                if (shape.label === "wall") {
+                    Body.setStatic(shape, false);
+                }
+            })
+        }
+    })
+})
